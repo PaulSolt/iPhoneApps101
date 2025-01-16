c007062cbf2804b118c2ae444d6c1cbea7435fab
Paul Solt <PaulSolt@gmail.com>
2025-01-16 16:32:38 -0500

    Updated Secret.swift

--- Secret.swift
+++ Secret.swift
@@ -2,5 +2,6 @@
 // Create an OpenWeather account and enter your API Key here
 // 1. Create an account: https://home.openweathermap.org/users/sign_up
 // 2. Copy your key from: https://home.openweathermap.org/api_keys
-// 3. Don't post this publically on Github (if you do your key may become overused by bots)
-let apiKey = "YOUR_API_KEY"
+// 3. Don't post this publically on Github
+
+let weatherAPIKey = "CHANGE_ME"
