adcabd955e41ade5d5077a57c9fed614fcb8f3b5
Paul Solt <PaulSolt@gmail.com>
2025-01-23 18:34:30 -0500

    Added starting point

---
+++ 2 - Design a Weather App/2.3 WeatherApp Design/WeatherApp-Begin/WeatherApp/DailyWeather.swift
@@ -0,0 +1,69 @@
+//
+//  DailyWeather.swift
+//  WeatherApp
+//
+//  Created by Paul Solt on 1/16/25.
+//
+
+import SwiftUI
+
+// UI Design
+// 1. Layout
+// 2. Design
+// 3. Cleanup
+
+struct DailyWeather: View {
+
+    @State var city: String = "Rochester"
+    @State var state: String = "NY"
+    @State var country: String = "US"
+
+    @State var selectedUnit: String = "F"
+
+    var body: some View {
+        VStack {
+            Image(systemName: "sun.max.fill") // smoke.fill
+            Text("Rochester")
+            Text("73.7ºF")
+            Text("Sunny")
+
+            VStack {
+                TextField("City", text: $city)
+                TextField("State (optional)", text: $state)
+                TextField("Country", text: $country)
+            }
+            .textFieldStyle(.roundedBorder)
+
+            Button {
+                print("Refresh!") // TODO: Make a network request for the weather
+            } label: {
+                Text("Refresh")
+            }
+
+            Spacer()
+        }
+        .onChange(of: city) { oldValue, newValue in
+            print("City: \(newValue)")
+        }
+        .overlay(alignment: .bottomTrailing, content: {
+//            Text("Cº") // Option + 0 = º
+            HStack {
+                Spacer()
+                Picker("Unit", selection: $selectedUnit) {
+                    ForEach(["C", "F"], id: \.self) { unit in
+                        Text("º\(unit)")
+                            .tag(unit)
+                    }
+                }
+                .pickerStyle(.segmented)
+                .frame(width: 80)
+            }
+        })
+//        .background(.yellow)
+        .padding()
+    }
+}
+
+#Preview {
+    DailyWeather()
+}
