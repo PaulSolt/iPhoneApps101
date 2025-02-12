bb02f6320796ba6afc1da4ddc8edc910b8172435
Paul Solt <PaulSolt@gmail.com>
2025-02-12 09:19:06 -0500

    Starting 2.7 Lesson

---
+++ 2 - Design a Weather App/2.7 Weather App - Swift Networking/WeatherApp-End/WeatherApp/WeatherService.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherService.swift
+//  WeatherApp
+//
+//  Created by Paul Solt on 2/12/25.
+//
+
+import Foundation
+
+struct WeatherService {
+
+    func fetchWeather() {
+        print("fetchWeather()")
+    }
+}
